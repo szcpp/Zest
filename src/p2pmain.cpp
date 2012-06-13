a31b750f81dd6c8d504d082f16718a3356b76df3
carbolymer <carbolymer@dropacid.pl>
2012-06-14 01:02:25 +0200

    Encoder

--- src/p2pmain.cpp
+++ src/p2pmain.cpp
@@ -1,14 +1,62 @@
 #include "Interface.hpp"
 #include "P2PServer.hpp"
 #include "P2PTest.hpp"
+#include "Encoder.hpp"
+
+char * Encrypt( char *key, char *message, int size)
+{
+ 
+        static char*    result;
+        int             n=0;
+        DES_cblock      ckey;
+        DES_key_schedule schedule;
+ 
+        result = ( char * ) malloc( size );
+ 
+        /* Prepare the key for use with DES_cfb64_encrypt */
+        memcpy( ckey, key,8);
+        DES_set_odd_parity( &ckey );
+        DES_set_key_checked( &ckey, &schedule );
+ 
+        /* Encryption occurs here */
+        DES_cfb64_encrypt( ( unsigned char * ) message, ( unsigned char * ) result,
+                           size, &schedule, &ckey, &n, DES_ENCRYPT );
+ 
+         return (result);
+}
+ 
+ 
+char * Decrypt( char *key, char *message, int size)
+{
+ 
+        static char*    result;
+        int             n=0;
+ 
+        DES_cblock      ckey;
+        DES_key_schedule schedule;
+ 
+        result = ( char * ) malloc( size );
+ 
+        /* Prepare the key for use with DES_cfb64_encrypt */
+        memcpy( ckey, key,8);
+        DES_set_odd_parity( &ckey );
+        DES_set_key_checked( &ckey, &schedule );
+ 
+        /* Decryption occurs here */
+        DES_cfb64_encrypt( ( unsigned char * ) message, ( unsigned char * ) result,
+                           size, &schedule, &ckey, &n, DES_DECRYPT );
+ 
+        return (result);
+ 
+}
 
 int main(int argc, char *argv[])
 {
-	P2PTest *t = new P2PTest;
-	P2PServer* c = new P2PServer;
-	// instaluje obserwatora w serwerze
-	c->addObserver(t);
-	c->start();
+	// P2PTest *t = new P2PTest;
+	// P2PServer* c = new P2PServer;
+	// // instaluje obserwatora w serwerze
+	// c->addObserver(t);
+	// c->start();
 
 	// nawiazuje polaczenie i wysyla wiadomosc
 	// P2PConnection* c = new P2PConnection("127.0.0.1");
@@ -18,5 +66,6 @@ int main(int argc, char *argv[])
 	// c->send(m);
 	// c->send(m);
 	// //c->closeSocket()
+
 	return 0;
 }
